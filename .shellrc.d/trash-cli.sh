8e4656d79d6a16d4f3313e8c6973b9aaeac32dcd
^x3ro <git@x3ro.dev>
2020-10-12 10:34:03 +0200

    👌 IMPROVE: Add more aliases for trash-cli

--- .shellrc.d/trash-cli.sh
+++ .shellrc.d/trash-cli.sh
@@ -1,10 +1,25 @@
 # trash-cli.sh
 
-function rm-disabled() {
+function __rm-disabled() {
     echo >&2 "WARN: Use \`rt\` instead of \`rm\` to move files to trash."
     echo >&2 "      Use \`\\rm\` if you really want to permanently delete files."
 }
 
-alias rt=trash-put
-alias rm=rm-disabled
+function __empty-trash() {
+    echo >&2 -n "Do you really want to PERMANENTLY delete all files in the trash bin? Type uppercase yes to confirm: "
+    read answer
 
+    if [[ ${answer} == "YES" ]]; then
+        trash-empty
+        return
+    fi
+    
+    echo >&2 "=> No files were deleted"
+}
+
+alias rm=__rm-disabled
+
+alias rt=trash-put
+alias rt-l=trash-list
+alias rt-empty=__empty-trash
+alias rt-rm=trash-rm
