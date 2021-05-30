e9ac8018e0aff9e528874c832e128e03afbbd5af
^x3ro <git@x3ro.dev>
2021-05-30 16:16:14 +0200

    👌 IMPROVE: Set EDITOR depending on what is available

--- .shellrc.d/editor-vim.sh
+++ .shellrc.d/editor-vim.sh
@@ -1,1 +1,18 @@
-export EDITOR=vim
+if command -v nvim >/dev/null; then
+    EDITOR=nvim
+elif command -v vim >/dev/null; then
+    EDITOR=vim
+else
+    echo >&2 -n '[WARN] `nvim`/`vim` not found. Falling back to '
+    if command -v vi >/dev/null; then
+        echo >&2 '`vi`'
+        EDITOR=vi
+    elif command -v nano >/dev/null; then
+        echo >&2 '`nano`'
+        EDITOR=nano
+    else
+        echo >&2 'NOTHING'
+    fi
+fi 
+
+export EDITOR
