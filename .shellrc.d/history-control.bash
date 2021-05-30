71074392a83c9b4fa551e8bc4e0ab09d4e36c0db
^x3ro <git@x3ro.dev>
2021-05-30 16:04:27 +0200

    📦 NEW: Configure Bash history

---
+++ .shellrc.d/history-control.bash
@@ -0,0 +1,8 @@
+# `ignorespace` - Ignore lines that start with a space
+# `ignoredups`  - Ignore duplicate commands
+# `ignoreboth`  - Same as `ignorespace:ignoredups`
+HISTCONTROL=ignoreboth
+
+HISTSIZE=50000
+HISTFILESIZE="$HISTSIZE"
+
