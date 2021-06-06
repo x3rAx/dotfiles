4b6779740516959ef61ebdb4f556fcde452277fd
^x3ro <git@x3ro.dev>
2021-06-06 02:34:45 +0200

    👌 IMPROVE: Add explanation for `erasedups`

--- .shellrc.d/history-control.bash
+++ .shellrc.d/history-control.bash
@@ -1,6 +1,7 @@
 # `ignorespace` - Ignore lines that start with a space
 # `ignoredups`  - Ignore duplicate commands
 # `ignoreboth`  - Same as `ignorespace:ignoredups`
+# `erasedups`   - Erase all duplicates in the whole history that match current line
 HISTCONTROL=ignoreboth
 
 HISTSIZE=50000
