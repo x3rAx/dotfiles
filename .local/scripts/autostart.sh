9a4a57e2365afced765de275555264696bd98652
^x3ro <git@x3ro.dev>
2021-11-08 18:06:23 +0100

    👌 IMPROVE: Sort autostart entries

--- .local/scripts/autostart.sh
+++ .local/scripts/autostart.sh
@@ -7,12 +7,12 @@ start() {
 start ~/.local/scripts/solaar-keys/solaar-key-daemon.sh
 
 start anydesk
+start birdtray
 start copyq
+start ferdi
+start keepassxc
 start signal-desktop --use-tray-icon # --start-in-tray
 start solaar --battery-icons solaar --window hide
-start telegram-desktop
 start syncthingtray
-start keepassxc
-start birdtray
+start telegram-desktop
 start thunderbird
-start ferdi
