f037a116af4a7f583b004c77cb61dc311ec671c3
^x3ro <git@x3ro.dev>
2022-01-20 11:14:54 +0100

    👌 IMPROVE: Enable preview in fzf

--- .shellrc.d/fzf.sh
+++ .shellrc.d/fzf.sh
@@ -3,7 +3,7 @@ FZF_DEFAULT_COMMAND='fd -H'
 FZF_DEFAULT_OPTS="--preview 'bat {}' --bind 'ctrl-u:half-page-up,ctrl-d:half-page-down,ctrl-h:page-up,ctrl-l:page-down,ctrl-f:preview-half-page-down,ctrl-b:preview-half-page-up'"
 export FZF_DEFAULT_COMMAND FZF_DEFAULT_OPTS
 
-FZF_CTRL_R_OPTS='--border'
+FZF_CTRL_R_OPTS='--border --preview ""'
 export FZF_CTRL_R_OPTS
 
 FZF_CTRL_T_COMMAND='fd -H'
