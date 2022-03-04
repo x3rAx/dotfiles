a1facb04afe8907db913b31ff46558e1456490a2
^x3ro <git@x3ro.dev>
2022-03-04 11:34:24 +0100

    👌 IMPROVE: bspwm: Sort autostart entries

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -2,19 +2,22 @@
 
 start() { "$@" & }
 
-bspc rule --add "Anydesk" desktop=8
-    start anydesk
+
+bspc rule --add "Anydesk"         desktop=8
+bspc rule --add "Thunderbird"     desktop=9
+bspc rule --add "Ferdi"           desktop=10
+bspc rule --add "Signal"          desktop=10
+bspc rule --add "TelegramDesktop" desktop=10
+
+
+start anydesk
 start birdtray
 start copyq
-bspc rule --add "Ferdi" desktop=10
-    start ferdi
+start ferdi
 start keepassxc
-bspc rule --add "Signal" desktop=10
-    start signal-desktop --use-tray-icon # --start-in-tray
+start kopia-ui-crashsafe
+start signal-desktop --use-tray-icon # --start-in-tray
 start solaar --battery-icons solaar --window hide
 start syncthingtray
-bspc rule --add "TelegramDesktop" desktop=9
-    start telegram-desktop
-bspc rule --add "Thunderbird" desktop=9
-    start thunderbird
-start kopia-ui-crashsafe
+start telegram-desktop
+start thunderbird
