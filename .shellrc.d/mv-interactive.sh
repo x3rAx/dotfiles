cec5d9f6e5c14ac761e141bc93ac0602e5259127
^x3ro <git@x3ro.dev>
2022-03-17 11:14:01 +0100

    👌 IMPROVE: Make interactive `mv` an executable to enable `sudo mvi`

--- .shellrc.d/mv-interactive.sh
+++ .shellrc.d/mv-interactive.sh
@@ -1,10 +1,3 @@
 function mv() {
-    local path="$1"
-    if [ "$#" -ne 1 ] || [ ! -e "$path" ]; then
-        command mv "$@"
-        return
-    fi
-
-    read -p '[mv]: ' -ei "$1" newpath
-    command mv -v -- "$path" "$newpath"
+    mvi "$@"
 }
