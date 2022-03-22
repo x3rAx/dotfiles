dc24ea474828f5c0d2615367fcf349b68cc1aa43
^x3ro <git@x3ro.dev>
2022-03-22 10:59:42 +0100

    👌 IMPROVE: Add rust example to `nixify`

--- .shellrc.d/nixify.sh
+++ .shellrc.d/nixify.sh
@@ -47,15 +47,25 @@ in pkgs.mkShell {
   # Programs and libraries used at *build*-time on the build host
   nativeBuildInputs = with pkgs; [
     bashInteractive
+
+    # ~~~ Rust ~~~
+    #cargo
+    #rustc
+    #rustfmt
+    #pkg-config # For finding openssl
+    #openssl.dev
+    # ~~~ / Rust ~~~
   ];
   # Programs and libraries used by the new derivation at *run*-time on the target host
   buildInputs = with pkgs; [
   ];
 
+  # ~~~ Rust ~~~
   # Certain Rust tools won't work without this
   # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
   # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
   #RUST_SRC_PATH = "\${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  # ~~~ / Rust ~~~
 }
 EOF
     ${EDITOR:-vim} shell.nix
