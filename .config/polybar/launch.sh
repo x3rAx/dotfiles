3c713fe3d54cfdb399146ddcd96e49787812db8a
^x3ro <git@x3ro.dev>
2022-04-06 00:37:50 +0200

    📦 NEW: polybar: Launch instances for each monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,56 @@
 #!/usr/bin/env bash
 
+## Terminate already running bar instances
+#killall -q polybar
+## If all your bars have ipc enabled, you can also use 
+## polybar-msg cmd quit
+#
+## Launch bar1 and bar2
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar -r main 2>&1 | tee -a /tmp/polybar1.log & disown
+##polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+#
+#echo "Bars launched..."
+#
+####################################
+
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar -r main 2>&1 | tee -a /tmp/polybar1.log & disown
-#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
-echo "Bars launched..."
+if ! type "xrandr"; then
+  polybar --reload primary &
+  exit
+fi
+
+
+get_connected() {
+    xrandr --query | grep " connected" | cut -d' ' -f1
+}
+
+
+get_primary() {
+    xrandr --query | grep " connected" | grep " primary" | head -n1 | cut -d' ' -f1
+}
+
+
+# Primary
+primary="$(get_primary)"
+
+log_tpl="/tmp/polybar-%s.$(date +%s).log"
+
+for monitor in $(get_connected); do
+    log="$(printf "$log_tpl" "$monitor")"
+
+    # If there is no primary monitor, fall back to using first one as primary
+    if [[ -z $primary ]]; then primary="$monitor"; fi
+
+    # Primary
+    if [[ $monitor == $primary ]]; then
+        MONITOR=$monitor polybar --reload primary >"$log" &
+        continue
+    fi
+
+    # Secondaries
+    MONITOR=$monitor polybar --reload secondary >"$log" &
+done
