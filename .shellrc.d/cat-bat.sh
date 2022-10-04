5ae2bfeef9108b446de6d2bcff169713f89f17f2
^x3ro <git@x3ro.dev>
2022-10-04 09:10:51 +0200

    📦 NEW: Use `bat` to colorize man pages

--- .shellrc.d/cat-bat.sh
+++ .shellrc.d/cat-bat.sh
@@ -1,6 +1,17 @@
+_bat_cmd=
+
 if [ "$(command -v bat)" ]; then
+    _bat_cmd=bat
     alias cat="bat"
 elif [ "$(command -v batcat)" ]; then
+    _bat_cmd=batcat
     alias cat="batcat"
     alias bat="batcat"
 fi
+
+if [[ -n $_bat_cmd ]]; then
+    # Use bat to colorize man pages 🤩
+    export MANPAGER="sh -c 'col -bx | ${_bat_cmd} -l man -p'"
+fi
+
+unset _bat_cmd
