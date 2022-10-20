298992021b2ef1c00896642127efea6546854139
^x3ro <git@x3ro.dev>
2022-10-20 09:17:37 +0200

    🐛 FIX: Use custom bash history file to prevent truncating

--- .shellrc.d/history-control.bash
+++ .shellrc.d/history-control.bash
@@ -7,3 +7,6 @@ HISTCONTROL=ignoreboth
 HISTSIZE=5000000
 HISTFILESIZE="$HISTSIZE"
 
+# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
+HISTFILE=~/.bash_history_long
