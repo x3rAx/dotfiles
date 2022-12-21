afc35e2eefd1df0dd48d2c8b76bd90a39af7ddcc
^x3ro <git@x3ro.dev>
2022-12-22 00:42:40 +0100

    👌 IMPROVE: Alias `ranger` to `ranger-cd`

--- .shellrc.d/ranger.sh
+++ .shellrc.d/ranger.sh
@@ -1,17 +1,16 @@
 function ranger-cd {
     local IFS=$'\t\n'
     local tempfile="$(mktemp -t tmp.XXXXXX)"
-    local ranger_cmd=(
-        command
-        ranger
-        --cmd="map Q chain shell echo %d > \"$tempfile\"; quitall"
-    )
 
-    "${ranger_cmd[@]}" "$@"
+    command ranger \
+        --cmd="map Q chain shell echo %d > \"$tempfile\"; quitall" \
+        "$@"
+
     if [[ -f "$tempfile" ]] && [[ "$(cat -- "$tempfile")" != $(pwd) ]]; then
         cd -- "$(cat -- "$tempfile")" || return
     fi
-    #command rm -f -- "$tempfile" 2>/dev/null
+    command rm -f -- "$tempfile" 2>/dev/null
 }
 
-alias r=ranger-cd
+alias ranger=ranger-cd
+alias r=ranger
