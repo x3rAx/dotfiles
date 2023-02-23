edf385b7ce99321edfb324226f64103a57d82ef0
^x3ro <git@x3ro.dev>
2023-02-23 17:26:40 +0100

    👌 IMPROVE: Change autostart apps

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -4,8 +4,9 @@ start() { "$@" & }
 
 
 bspc rule --add "Anydesk"         desktop=8
-bspc rule --add "Rustdesk"        desktop=8  state=tiled
+#bspc rule --add "Rustdesk"        desktop=8  state=tiled
 bspc rule --add "Thunderbird"     desktop=9
+bspc rule --add "discord"         desktop=9
 bspc rule --add "Ferdi"           desktop=10
 bspc rule --add "Signal"          desktop=10
 bspc rule --add "TelegramDesktop" desktop=10
@@ -17,9 +18,12 @@ start copyq
 start ferdi
 start keepassxc
 start kopia-ui-crashsafe
-start rustdesk
+#start rustdesk
 start signal-desktop --use-tray-icon # --start-in-tray
 start solaar --battery-icons solaar --window hide
 start syncthingtray
 start telegram-desktop
 start thunderbird
+start barrier # mouse / keyboard sharing
+start discord
+start steam -silent
