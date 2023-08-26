75a35355ff3316fd22acdd438a59cb3d8126530d
^x3ro <git@x3ro.dev>
2023-08-26 16:40:15 +0200

    📦 NEW: Add shell aliases file

---
+++ .shellrc.d/aliases.sh
@@ -0,0 +1,6 @@
+# For config aliases, have a look at
+#   ./config-aliases.sh
+
+# Misc
+alias g='git'; complete -o bashdefault -o default -o nospace -F __git_wrap__git_main g
+alias tmate='env -u TMUX tmate'
