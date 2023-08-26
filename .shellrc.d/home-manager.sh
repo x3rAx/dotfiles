7f7a1cebbd876af2275b56733c5cd0376ba5244b
^x3ro <git@x3ro.dev>
2023-08-26 16:25:27 +0200

    📦 NEW: Source home-manager session variables

---
+++ .shellrc.d/home-manager.sh
@@ -0,0 +1,2 @@
+# "If you do not plan on having Home Manager manage your shell configuration then you must source the [following] file in your shell configuration."
+source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
