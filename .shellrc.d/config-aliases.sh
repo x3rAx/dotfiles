b8cb352e8539e98bfeea1575518531b4eafb7d5f
^x3ro <git@x3ro.dev>
2023-08-26 16:34:58 +0200

    🐛 FIX: Use new file name in config-aliases alias

--- .shellrc.d/config-aliases.sh
+++ .shellrc.d/config-aliases.sh
@@ -1,6 +1,6 @@
 _editor=nvim
 
-alias ,="$_editor ~/.shellrc.d/aliases.sh"
+alias ,="$_editor ~/.shellrc.d/config-aliases.sh"
 alias ,hyprland="$_editor -c 'cd ~/.config/hypr' ~/.config/hypr/hyprland.conf"
 alias ,eww="$_editor -c 'cd ~/.config/eww' ~/.config/eww/eww.yuck"
 alias ,nix="$_editor -c 'cd /etc/nixos' /etc/nixos/current-host/configuration.nix"
