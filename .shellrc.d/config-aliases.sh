17b808d1b0714adc2e26ffcf6c3d173e31d7d718
^x3ro <git@x3ro.dev>
2023-08-26 16:38:49 +0200

    📦 NEW: Merge in local bspwm config aliases

--- .shellrc.d/config-aliases.sh
+++ .shellrc.d/config-aliases.sh
@@ -1,6 +1,13 @@
 _editor=nvim
 
 alias ,="$_editor ~/.shellrc.d/config-aliases.sh"
+
+# Bspwm
+alias ,bspwm="$_editor ~/.config/bspwm/bspwmrc +':cd %:h'"
+alias ,sxhkd="$_editor ~/.config/sxhkd/sxhkdrc +':cd %:h'"
+alias ,polybar="$_editor ~/.config/polybar/config +':cd %:h'"
+
+# Hyprland
 alias ,hyprland="$_editor -c 'cd ~/.config/hypr' ~/.config/hypr/hyprland.conf"
 alias ,eww="$_editor -c 'cd ~/.config/eww' ~/.config/eww/eww.yuck"
 alias ,nix="$_editor -c 'cd /etc/nixos' /etc/nixos/current-host/configuration.nix"
