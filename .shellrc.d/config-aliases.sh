ff8820386067e8b8b12bd6f730e5a65975c174a2
^x3ro <git@x3ro.dev>
2023-08-26 16:32:18 +0200

    👌 IMPROVE: Add config aliases using comma prefix

--- .shellrc.d/config-aliases.sh
+++ .shellrc.d/config-aliases.sh
@@ -1,5 +1,9 @@
 _editor=nvim
 
-alias h="$_editor -c 'cd ~/.config/hypr' ~/.config/hypr/hyprland.conf"
+alias ,="$_editor ~/.shellrc.d/aliases.sh"
+alias ,hyprland="$_editor -c 'cd ~/.config/hypr' ~/.config/hypr/hyprland.conf"
+alias ,eww="$_editor -c 'cd ~/.config/eww' ~/.config/eww/eww.yuck"
+alias ,nix="$_editor -c 'cd /etc/nixos' /etc/nixos/current-host/configuration.nix"
+alias ,autostart="$_editor -c 'cd ~/.local/scripts' ~/.local/scripts/autostart.sh"
 
 unset _editor
