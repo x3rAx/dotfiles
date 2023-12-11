42456c37566d6eacdcb6e3eb38c5f9bbdd85d4d0
^x3ro <git@x3ro.dev>
2023-12-11 13:23:39 +0100

    👌 IMPROVE: Add completions for distrobox alias

--- .shellrc.d/distrobox.sh
+++ .shellrc.d/distrobox.sh
@@ -12,7 +12,25 @@ dbox() {
 
 __dbox() {
     __load_completion distrobox
+
+    if [[ $COMP_CWORD == 1 ]]; then
+        __distrobox
+        COMPREPLY=(
+            "${COMPREPLY[@]}"
+            $(compgen -W "e ls" "${COMP_WORDS[$COMP_CWORD]}")
+        )
+        return
+    fi
+
+    if [[ $COMP_CWORD -gt 1 ]]; then
+        case "${COMP_WORDS[1]}" in
+            'e') COMP_WORDS[1]='enter';;
+            'ls') COMP_WORDS[1]='list';;
+        esac
+    fi
+
     __distrobox
 }
 
 complete -F __dbox dbox
+complete -F __dbox distrobox
