fa11d42a43190715f0a5f844072b2ce8e624af58
^x3ro <git@x3ro.dev>
2023-12-27 16:04:57 +0100

    📦 NEW: Set indentation to 4 spaces

--- .config/nvchad/init.lua
+++ .config/nvchad/init.lua
@@ -1,5 +1,14 @@
 local opt = vim.opt
+local g = vim.g
 
 -- Numbers
 opt.relativenumber = true
+
+-- Indenting
+local indent = 4
+opt.expandtab = true
+opt.shiftwidth = indent
+opt.smartindent = true
+opt.tabstop = indent
+opt.softtabstop = indent
 
