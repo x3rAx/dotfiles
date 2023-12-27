ed877afdecbdde6369ccfef8d25af7f795bfe843
^x3ro <git@x3ro.dev>
2023-12-27 16:10:22 +0100

    📦 NEW: Configure treesitter languages

--- .config/nvchad/plugins.lua
+++ .config/nvchad/plugins.lua
@@ -3,6 +3,16 @@ local plugins = {
     { "github/copilot.vim",
         lazy = false,
     },
+
+    -- Syntax highlighting
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "c", "lua", "rust", "vim", "vimdoc",
+            },
+        },
+    },
 }
 
 return plugins
