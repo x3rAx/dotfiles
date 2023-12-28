e4fd0b0a00459db1eaa9ca541e82d81fd08c43ec
^x3ro <git@x3ro.dev>
2023-12-28 05:14:47 +0100

    👌 IMPROVE: Set themes for light/dark toggle

--- .config/nvchad/chadrc.lua
+++ .config/nvchad/chadrc.lua
@@ -1,7 +1,10 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'catppuccin' }
+M.ui = {
+    theme = 'catppuccin',
+    theme_toggle = { "catppuccin", "one_light" },
+}
 
 M.plugins = 'custom.plugins'
 
