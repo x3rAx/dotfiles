759e3fd1efd09d7af1b6d27c12f99b0e2fc09000
^x3ro <git@x3ro.dev>
2023-12-31 17:48:28 +0100

    📦 NEW: Show current line and column in cursor_position module

--- .config/nvchad/chadrc.lua
+++ .config/nvchad/chadrc.lua
@@ -10,7 +10,19 @@ M.ui = {
             fg = "white",
             bg = "statusline_bg"
         },
-    }
+    },
+
+    statusline = {
+        overriden_modules = function(modules)
+            -- Show current line and column number in the cursor_position
+            -- module. This replaces the `vim.opt.ruler` option.
+            modules[10] = (function ()
+                -- Split the string at the first highlight group after the icon
+                local icon, hl_txt, text = modules[10]:match("^(%S+%s+)(%%#.+#) (.*)")
+                return icon .. hl_txt .. " %l,%c | " .. hl_txt .. text
+            end)()
+        end,
+    },
 }
 
 M.plugins = 'custom.plugins'
