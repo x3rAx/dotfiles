6d26586b90e2b5040580133d1f61913cc56c01a1
^x3ro <git@x3ro.dev>
2024-01-02 13:53:22 +0100

    👌 IMPROVE: Add examples for how to extend lspconfig

--- .config/nvchad/configs/lspconfig.lua
+++ .config/nvchad/configs/lspconfig.lua
@@ -3,6 +3,15 @@ local M = require("plugins.configs.lspconfig")
 local lspconfig = require("lspconfig")
 local root_pattern = lspconfig.util.root_pattern
 
+-- Extend the on_attach function
+--M.on_attach = function(client, bufnr)
+--    require("plugins.configs.lspconfig").on_attach(client, bufnr)
+--
+--    --if client.server_capabilities.documentSymbolProvider then
+--    --    require('nvim-navic').attach(client, bufnr)
+--    --end
+--end
+
 -- Servers to auto-setup
 local servers = {
     "nil_ls", -- Nix
@@ -16,5 +25,19 @@ for _, lsp in ipairs(servers) do
         capabilities = M.capabilities,
     }
 end
+
+-- Manual server setup
+--lspconfig.sumneko_lua.setup {
+--    on_attach = on_attach,
+--    capabilities = M.capabilities,
+--    cmd = { "lua-language-server" },
+--    settings = {
+--        Lua = {
+--            diagnostics = {
+--                globals = { "vim" },
+--            },
+--        },
+--    },
+--}
 
 return M
