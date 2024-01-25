1b3aaae1f0f24f1724f525a10c6cade0ccfdb46c
^x3ro <git@x3ro.dev>
2024-01-25 22:05:46 +0100

    👌 IMPROVE: Sync with 'primary' clipboard

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- TODO: Maybe set this to "" (empty) to completely disable automatic clipboard sync
+opt.clipboard = "unnamed" -- Sync with 'primary' clipboard (selection / middle-mouse clipboard)
