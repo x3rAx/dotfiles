f943cdfbd9f7be3102f43fff70705cd991b943a7
^x3ro <git@x3ro.dev>
2024-01-28 12:19:12 +0100

    👌 IMPROVE: Add fix for LazyVim autocmd 'last_loc'
    When exiting vim without saving the current buffer, the last location
    might be in a line number that's greater thatn the number of lines
    in the buffer. In this case, the 'last_loc' autocmd provided by LazyVim
    will not set the location and the cursor will be placed at the first
    line. I find it more intuitive to place the cursor at the last line in
    this case.

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,30 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("x3ro_" .. name, { clear = true })
+end
+
+--- Fix for LazyVim autocommand to go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function(event)
+    local buf = event.buf
+    if not vim.b[buf].lazyvim_last_loc or vim.b[buf].x3ro_last_loc then
+      return
+    end
+
+    vim.b[buf].x3ro_last_loc = true
+
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+
+    -- The actual fix: If the mark line is greater than the number of lines in
+    -- the buffer, go to the last line
+    if mark[1] > lcount then
+      mark[1] = lcount
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
