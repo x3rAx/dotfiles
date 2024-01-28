31c7c987144207b4e6733f55db3dcb85315a671d
^x3ro <git@x3ro.dev>
2024-01-28 12:29:36 +0100

    👌 IMPROVE: Reduce vim plugin update check frequency to once per day

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -27,7 +27,10 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = {
+    enabled = true, -- automatically check for plugin updates
+    frequency = 24 * 60 * 60, -- check for updates once a day
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
