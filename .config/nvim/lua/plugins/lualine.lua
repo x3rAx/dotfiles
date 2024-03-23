03b9cec6822f9f3b578e778f4e3fae5d621f1ded
^x3ro <git@x3ro.dev>
2024-03-23 03:41:24 +0100

    📦 NEW: Add VIM icon to current "mode" status

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,9 +1,27 @@
+local function get_hlgroup_color(group, attr)
+  return vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID(group)), attr)
+end
+
+local function get_mode_seperator_color(attr)
+  return function(_)
+    return { [attr] = get_hlgroup_color("PmenuSel", "bg") }
+  end
+end
+
 return {
   {
     "nvim-lualine/lualine.nvim",
     opts = {
       options = {
         section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+            icon = "",
+          },
+        },
       },
     },
   },
