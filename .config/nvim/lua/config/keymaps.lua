46b28eafddac7b9e108fe100e0c885f94700b1a6
^x3ro <git@x3ro.dev>
2024-05-19 23:35:16 +0200

    👌 IMPROVE: Delete buffer only using `ZX`

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,6 +9,7 @@ local map = vim.keymap.set
 
 -------------------------------------------------------------------------------
 --- Undo / Redo
+---
 map({ "n", "i" }, "<C-z>", function()
   vim.cmd("undo")
 end, { desc = "Undo" })
@@ -18,22 +19,23 @@ end, { desc = "Redo" })
 
 -------------------------------------------------------------------------------
 --- Copy to system clipboard
+---
 map({ "v" }, "<C-c>", '"+y', { desc = "Copy to system clipboard" })
 
 -------------------------------------------------------------------------------
---- Close Buffer / Window
-map({ "n" }, "<leader>X", "<Space>bd", { desc = "Delete Buffer", remap = true })
-map({ "n" }, "zx", "<Space>bd", { desc = "Delete Buffer", remap = true })
-map({ "n" }, "<leader>Q", "<C-W>c", { desc = "Delete Window" })
-map({ "n" }, "zq", "<C-W>c", { desc = "Delete Window" })
+--- Close Buffer
+---
+map({ "n" }, "ZX", "<leader>bd", { desc = "Delete Buffer", remap = true })
 
 -------------------------------------------------------------------------------
 --- Comments
+---
 map({ "n" }, "<c-/>", "gcc", { desc = "Comment line", remap = true })
 map({ "v" }, "<c-/>", "gc", { desc = "Comment selected lines", remap = true })
 
 -------------------------------------------------------------------------------
 --- Floating Terminal
+---
 local lazyterm = function()
   Util.terminal(nil, { cwd = Util.root() })
 end
@@ -42,10 +44,12 @@ map("t", "<m-`>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 
 -------------------------------------------------------------------------------
 --- Run current buffer with vim-lua
+---
 -- NOTE: This must be a vimscript mapping because otherwise only the last
 --       `print` statement of the executed buffer is shown
 map({ "n" }, "<leader>rl", ':lua require("lib.exec-lua-buf")()<CR>', { desc = "Run current buffer with vim-lua" })
 
 -------------------------------------------------------------------------------
 --- Misc
+---
 map({ "i" }, "<C-BS>", "<C-w>", { desc = "Delete word to the left" })
