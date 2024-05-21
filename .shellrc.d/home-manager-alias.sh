38f9f98ff5c68d5762f327030e4477f00f657416
^x3ro <git@x3ro.dev>
2024-05-21 12:44:08 +0200

    👌 IMPROVE: Format code

--- .shellrc.d/home-manager-alias.sh
+++ .shellrc.d/home-manager-alias.sh
@@ -1,18 +1,18 @@
 hm() {
-    if [ "$1" == "e" ]; then
-        shift
-        set -- edit "$@"
-    elif [ "$1" == "s" ]; then
-        shift
-        set -- switch "$@"
-    fi
+	if [ "$1" == "e" ]; then
+		shift
+		set -- edit "$@"
+	elif [ "$1" == "s" ]; then
+		shift
+		set -- switch "$@"
+	fi
 
-    home-manager "$@"
+	home-manager "$@"
 }
 
 __hm() {
-    __load_completion home-manager
-    _home-manager_completions
+	__load_completion home-manager
+	_home-manager_completions
 }
 
 complete -F __hm hm
