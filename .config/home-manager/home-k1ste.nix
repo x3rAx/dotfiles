b9c82fd76829aefca70ae176813dfc68ef79be85
^x3ro <git@x3ro.dev>
2024-09-07 18:42:40 +0200

    👌 IMPROVE: Remove example comment from packages config

--- .config/home-manager/home-k1ste.nix
+++ .config/home-manager/home-k1ste.nix
@@ -90,23 +90,6 @@ in {
   # environment.
   home.packages =
     (with pkgs; [
-      # # Adds the 'hello' command to your environment. It prints a friendly
-      # # "Hello, world!" when run.
-      # pkgs.hello
-
-      # # It is sometimes useful to fine-tune packages, for example, by applying
-      # # overrides. You can do that directly here, just don't forget the
-      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-      # # fonts?
-      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-      # # You can also create simple shell scripts directly inside your
-      # # configuration. For example, this adds a command 'my-hello' to your
-      # # environment:
-      # (pkgs.writeShellScriptBin "my-hello" ''
-      #   echo "Hello, ${config.home.username}!"
-      # '')
-
       age
       android-tools
       unstable.armagetronad # Game
@@ -206,7 +189,6 @@ in {
       xss-lock
       xorg.xwininfo
       unstable.zoxide
-
     ]);
 
   # Home Manager can also manage your environment variables through
