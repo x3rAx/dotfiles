e32ef122bbb36e72c45cbc0696b06ebdbb183687
^x3ro <git@x3ro.dev>
2024-09-07 02:47:16 +0200

    🐛 FIX: home: Update NixOS/nixpkgs url

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
