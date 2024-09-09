24ceab59bd7dde43f04c4f815d169c997270d084
^x3ro <git@x3ro.dev>
2024-09-10 00:23:33 +0200

    📦 NEW: yazi: Show user/group in status bar

---
+++ .config/yazi/init.lua
@@ -0,0 +1,17 @@
+
+
+--- Show User/Group in Status Bar ---
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line {}
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	}
+end, 500, Status.RIGHT)
+
