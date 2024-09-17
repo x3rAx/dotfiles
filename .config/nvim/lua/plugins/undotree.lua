4b0f3ff8324856b8e1fd9b2dd2edda1ae716038f
^x3ro <git@x3ro.dev>
2024-09-18 00:38:52 +0200

    📦 NEW: nvim: Add undo-tree

---
+++ .config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "mbbill/undotree",
+    init = function()
+      vim.g.undotree_WindowLayout = 3
+      vim.g.undotree_SplitWidth = 40
+      vim.g.undotree_SetFocusWhenToggle = 1
+      vim.g.undotree_TreeNodeShape = ''
+      vim.g.undotree_DiffCommand = [[bash -c 'diff -u --color "$@" | grep -vE "^(---|\\+\\+\\+)"' -s]]
+    end,
+    keys = {
+      {
+        "<leader>uu",
+        function()
+          vim.cmd.UndotreeToggle()
+        end
+      },
+    },
+  },
+}
