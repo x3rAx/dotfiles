c1d0b08065955b313171a38cd182763f9c965478
^x3ro <git@x3ro.dev>
2024-09-30 18:58:34 +0200

    📦 NEW: config-aliases: Restore vim session

--- .shellrc.d/config-aliases.sh
+++ .shellrc.d/config-aliases.sh
@@ -1,16 +1,45 @@
+# Usage
+#   ,edit <directory> [<vim-args>]
+#   ,edit <file>
+#
+# When passing a directory, the directory will be opened in neovim and the
+# last session of this directory will be loaded. When passing additional
+# <vim-args>, they are passed to nvim. This can be used to specify a file
+# that should be opened by default when no session exists.
+#
+# When passing in a file as first argument, the file will be opened and no
+# session will be loaded.
+,edit() {
+    if [[ ! -d "$1" ]]; then
+        vim "$@"
+        return
+    fi
+
+    pushd "$1" >/dev/null
+    shift
+    nvim -c "lua require'persistence'.load(); vim.cmd('Neotree show')" "$@"
+    popd >/dev/null
+}
+
 _editor=nvim
 
-alias ,="$_editor ~/.shellrc.d/config-aliases.sh"
+alias ,=",edit ~/.shellrc.d/config-aliases.sh"
 
 # Bspwm
-alias ,bspwm="$_editor ~/.config/bspwm/bspwmrc +':cd %:h'"
-alias ,sxhkd="$_editor ~/.config/sxhkd/sxhkdrc +':cd %:h'"
-alias ,polybar="$_editor ~/.config/polybar/config +':cd %:h'"
+alias ,bspwm=",edit ~/.config/bspwm bspwmrc"
+alias ,sxhkd=",edit ~/.config/sxhkd sxhkdrc"
+alias ,polybar=",edit ~/.config/polybar config"
+alias ,picom=",edit ~/.config/picom picom.conf"
 
 # Hyprland
-alias ,hyprland="$_editor -c 'cd ~/.config/hypr' ~/.config/hypr/hyprland.conf"
-alias ,eww="$_editor -c 'cd ~/.config/eww' ~/.config/eww/eww.yuck"
-alias ,nix="$_editor -c 'cd /etc/nixos' /etc/nixos/current-host/configuration.nix"
-alias ,autostart="$_editor -c 'cd ~/.local/scripts' ~/.local/scripts/autostart.sh"
+alias ,hyprland=",edit ~/.config/hypr hyprland.conf"
+alias ,eww=",edit ~/.config/eww eww.yuck"
+
+# Others
+alias ,nvim=",edit ~/.config/nvim"
+alias ,home-manager=",edit ~/.config/home-manager home.nix"
+alias ,flatpak-aliases=",edit ~/.shellrc.d/flatpak-aliases.sh"
+alias ,nixos="sudo $_editor -c 'cd /etc/nixos/' /etc/nixos/current-host/configuration.nix"
+alias ,autostart=",edit ~/.local/scripts autostart.sh"
 
 unset _editor
