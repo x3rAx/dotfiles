26071290694f253c291b48ee6ba2f0b7e8a8ba19
^x3ro <git@x3ro.dev>
2024-10-21 16:38:28 +0200

    🐛 FIX: nvim: Disable `vtsls` when `denols` should be active

--- .config/nvim/lua/plugins/deno.lua
+++ .config/nvim/lua/plugins/deno.lua
@@ -1,7 +1,21 @@
 local root_pattern = require("lspconfig.util").root_pattern
 
+local function is_deno_exe_file(fname)
+  local file = io.open(fname)
+  if not file then
+    return false
+  end
+
+  -- local lines = file:lines()
+  -- local first_line = lines()
+  local first_line = file:read("*l")
+
+  -- Test if shebang matches deno
+  return first_line:match("^#!/usr/bin/env deno") or first_line:match("^#!/usr/bin/env %-S deno")
+end
+
 local function get_deno_root_dir(fname)
-  -- Root pattern from: https://github.com/neovim/nvim-lspconfig/blob/fd49d5863e873891be37afac79b1f56fb34bb5d3/doc/configs.md?plain=1#L2986
+  -- Root pattern from: https://github.com/neovim/nvim-lspconfig/blob/fd49d586/doc/configs.md?plain=1#L2986
   local root = root_pattern("deno.json", "deno.jsonc", ".git")(fname)
   if root then
     return root
@@ -9,41 +23,40 @@ local function get_deno_root_dir(fname)
 
   -- Single-file support --
 
-  local file = io.open(fname)
-  if file == nil then
+  -- Test if shebang matches deno
+  if not is_deno_exe_file(fname) then
     return nil
   end
 
-  local lines = file:lines()
-  local first_line = lines()
+  -- Get a directory with a deno.json for single-file LSP support
+  local dir = vim.fn.stdpath("config") .. "/deno-single-file-lsp-root/"
+  -- Get the directory of the current file
+  -- local dir = vim.fn.fnamemodify(fname, ":h")
 
-  -- Test if shebang matches deno
-  if first_line:match("^#!/usr/bin/env deno") or first_line:match("^#!/usr/bin/env %-S deno") then
-    -- Get the directory of the current file
-    -- local dir = vim.fn.fnamemodify(fname, ":h")
+  -- vim.notify("Deno LPS single-file enabled")
 
-    -- Get a directory with a deno.json for single-file LSP support
-    local dir = vim.fn.stdpath("config") .. "/deno-single-file-lsp-root/"
-
-    -- HACK: Disable conflicting "vtsls". Maybe this should be done in the `config` function?
-    LazyVim.lsp.disable("vtsls", function()
-      return true
-    end)
-
-    return dir
-  end
-
-  return nil
+  return dir
 end
 
 return {
   {
     "neovim/nvim-lspconfig",
     lazy = true,
+
     opts = {
       servers = {
         denols = {
           root_dir = get_deno_root_dir,
+        },
+
+        -- Disable vtsls when deno is detected
+        vtsls = {
+          on_attach = function(client, bufnr)
+            local fname = vim.api.nvim_buf_get_name(bufnr)
+            if get_deno_root_dir(fname) then
+              vim.lsp.stop_client(client.id)
+            end
+          end,
         },
       },
     },
