59ed9e76f58ccaa06c8d192dbf9a745267a1ffac
^x3ro <git@x3ro.dev>
2024-10-27 14:31:55 +0100

    📦 NEW: Move from starship to oh-my-posh

--- .shellrc.d/Z95-starship.bash
+++
@@ -1,2 +0,0 @@
-eval "$(starship completions bash)"
-eval "$(starship init bash)"
---
+++ .shellrc.d/oh-my-posh.bash
@@ -0,0 +1,2 @@
+echo '' # HACK: Oh-My-Posh ships the first newline, so we add one one ourselves
+eval "$(oh-my-posh init bash --config ~/.config/oh-my-posh/config.toml)"
