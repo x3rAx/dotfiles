1187f4ebb694a6840b5486c6713f5511f18a0dd3
^x3ro <git@x3ro.dev>
2024-11-22 23:51:38 +0100

    👌 IMPROVE: [fzf] Disable configuration

--- .shellrc.d/fzf.sh
+++ .shellrc.d/fzf.sh
@@ -1,16 +1,16 @@
-FZF_DEFAULT_COMMAND='fd -H'
-#FZF_DEFAULT_OPTS='--border'
-FZF_DEFAULT_OPTS="--preview 'bat {}' --bind 'ctrl-u:half-page-up,ctrl-d:half-page-down,ctrl-h:page-up,ctrl-l:page-down,ctrl-f:preview-half-page-down,ctrl-b:preview-half-page-up'"
-export FZF_DEFAULT_COMMAND FZF_DEFAULT_OPTS
-
-FZF_CTRL_R_OPTS='--border --preview ""'
-export FZF_CTRL_R_OPTS
-
-FZF_CTRL_T_COMMAND='fd -H'
-FZF_CTRL_T_OPTS='--border'
-export FZF_CTRL_T_COMMAND FZF_CTRL_T_OPTS
-
-FZF_ALT_C_COMMAND='fd -H -t d'
-FZF_ALT_C_OPTS='--border'
-export FZF_ALT_C_COMMAND FZF_ALT_C_OPTS
+#FZF_DEFAULT_COMMAND='fd -H'
+##FZF_DEFAULT_OPTS='--border'
+#FZF_DEFAULT_OPTS="--preview 'bat {}' --bind 'ctrl-u:half-page-up,ctrl-d:half-page-down,ctrl-h:page-up,ctrl-l:page-down,ctrl-f:preview-half-page-down,ctrl-b:preview-half-page-up'"
+#export FZF_DEFAULT_COMMAND FZF_DEFAULT_OPTS
+#
+#FZF_CTRL_R_OPTS='--border --preview ""'
+#export FZF_CTRL_R_OPTS
+#
+#FZF_CTRL_T_COMMAND='fd -H'
+#FZF_CTRL_T_OPTS='--border'
+#export FZF_CTRL_T_COMMAND FZF_CTRL_T_OPTS
+#
+#FZF_ALT_C_COMMAND='fd -H -t d'
+#FZF_ALT_C_OPTS='--border'
+#export FZF_ALT_C_COMMAND FZF_ALT_C_OPTS
 
