bda0bc8f5727e5855def81c02a4106d6364a3a4c
^x3ro <git@x3ro.dev>
2025-01-18 02:23:36 +0100

    🚀 RELEASE: Update flake to NixPkgs 24.11

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -12,7 +12,7 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     flakify = {
@@ -21,7 +21,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
