115ad93d5e5130ef1884b095924fbd11ce078f90
^x3ro <git@x3ro.dev>
2025-03-07 00:04:52 +0100

    git subrepo pull (merge) .tmux/plugins/tpm --force
    subrepo:
      subdir:   ".tmux/plugins/tpm"
      merged:   "99469c4a9"
    upstream:
      origin:   "https://github.com/tmux-plugins/tpm"
      branch:   "master"
      commit:   "99469c4a9"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

--- .tmux/plugins/tpm/scripts/helpers/tmux_utils.sh
+++ .tmux/plugins/tpm/scripts/helpers/tmux_utils.sh
@@ -1,5 +1,5 @@
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$CURRENT_DIR/plugin_functions.sh"
+HELPERS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$HELPERS_DIR/plugin_functions.sh"
 
 reload_tmux_environment() {
 	tmux source-file $(_get_user_tmux_conf) >/dev/null 2>&1
