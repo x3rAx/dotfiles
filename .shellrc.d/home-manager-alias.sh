4992cce4768e109e37080b0042b6f3f440ceb1f0
^x3ro <git@x3ro.dev>
2025-03-07 00:11:32 +0100

    👌 IMPROVE: [hm] Format code

--- .shellrc.d/home-manager-alias.sh
+++ .shellrc.d/home-manager-alias.sh
@@ -1,30 +1,30 @@
 hm() {
-	local XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+    local XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
     local hm_dir="${XDG_CONFIG_HOME}/home-manager"
 
-	if [ "$1" = 'u' ] || [ "$1" = 'update' ]; then
-		if [ ! -f "${hm_dir}/flake.nix" ]; then
-			echo "No flake.nix found in \"${XDG_CONFIG_HOME}/home-manager/\""
-			return
-		fi
-		nix flake update "${XDG_CONFIG_HOME}/home-manager/"
-		home-manager switch
-		return
-	fi
+    if [ "$1" = 'u' ] || [ "$1" = 'update' ]; then
+        if [ ! -f "${hm_dir}/flake.nix" ]; then
+            echo "No flake.nix found in \"${XDG_CONFIG_HOME}/home-manager/\""
+            return
+        fi
+        nix flake update "${XDG_CONFIG_HOME}/home-manager/"
+        home-manager switch
+        return
+    fi
 
-	if [ "$1" = "e" ] || [ "$1" = "edit" ]; then
-		shift
+    if [ "$1" = "e" ] || [ "$1" = "edit" ]; then
+        shift
         pushd "$hm_dir" >/dev/null
-		set -- edit "$@"
+        set -- edit "$@"
         home-manager "$@"
         popd >/dev/null
         return
     fi
-	
-    if [ "$1" = "s" ]; then
-		shift
-		set -- switch "$@"
-	fi
 
-	home-manager "$@"
+    if [ "$1" = "s" ]; then
+        shift
+        set -- switch "$@"
+    fi
+
+    home-manager "$@"
 }
